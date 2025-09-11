2442eb99c6e926dd79f814ee2488617ef1565c10
Pedro Rodrigues <pedro.rodrigues@josapar.com.br>
2025-09-11 14:56:30 -0300

    🎉 Beguin Structure Project

--- FirstMVCProject/Startup.cs
+++ FirstMVCProject/Startup.cs
@@ -1,16 +1,46 @@
-namespace FirstMVCProject;
+namespace LanchesMac;
+
 public class Startup
 {
-    public void ConfigureServices(IServiceCollection services)
+    public Startup(IConfiguration configuration)
     {
-        // Configurações de DI aqui
-        services.AddControllers();
+        Configuration = configuration;
     }
 
-    public void Configure(IApplicationBuilder app)
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
     {
-        // Pipeline de middleware aqui
+        services.AddControllersWithViews();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            //Pagina que mostra detalhes dos erros
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
         app.UseRouting();
-        app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        });
     }
 }
--- FirstMVCProject/obj/Release/net9.0/FirstMVCProject.AssemblyInfo.cs
+++ FirstMVCProject/obj/Release/net9.0/FirstMVCProject.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstMVCProject")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b499821c395916c5a17c0cc1e5d1efe94635847")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstMVCProject")]
